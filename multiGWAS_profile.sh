389434d5f82301707aa12c2d78f1ead75715abd5
Luis Garreta <lgarreta@gmail.com>
2021-03-28 16:41:40 -0500

    Removed MG profile

--- multiGWAS_profile.sh
+++
@@ -1,7 +0,0 @@
-
-#------------------- multiGWAS.R profile ---------------------
-export MULTIGWAS_HOME=/home/lg/agrosavia/GWAS-TOOL/repos/MultiGWASpre
-MULTIGWAS_TOOLS=$MULTIGWAS_HOME/opt/tools
-MULTIGWAS_JAVA=$MULTIGWAS_HOME/opt/jre/bin
-MULTIGWAS_MAIN=$MULTIGWAS_HOME/main
-export PATH=$MULTIGWAS_TOOLS:$MULTIGWAS_MAIN:$MULTIGWAS_JAVA:$PATH:
